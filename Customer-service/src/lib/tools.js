5771bfd14db23cd0eaa4026078bcdc24db8adcdb
T-jegou <thomas.jegou1@gmail.com>
2023-01-17 11:12:45 +0100

    Adapt async func to find by email and check password

--- Customer-service/src/lib/tools.js
+++ Customer-service/src/lib/tools.js
@@ -1,30 +1,38 @@
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const {userSchema} = require('../models/User');
+
+const User = mongoose.model('User', userSchema);
 
 async function hashPassword(password) {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     return hash;
-}
+};
 
 
 async function validatePassword(password, hashedPassword) {
     const isValid = await bcrypt.compare(password, hashedPassword);
     return isValid;
-}
+};
 
-async function isUserExistAndPasswordCorrect(email, password) {
-    User.findById(email, async (err, user) => {
-        if (err) {
-            return false
-        } else {
-          if (await validatePassword(password, user.password))  { 
-            return user;
-          } else {
-            return false
-          }
-        }
-      });
-}
+async function isUserExistAndPasswordCorrect(userEmail, password) {
+  try {
+    let user = await User.findOne({email: userEmail});
+    if (typeof user === "object") {
+      if (await validatePassword(password, user.password)) {
+        return user;
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  } catch (err) {
+    return false
+  }
+
+};
 
 module.exports = {
     isUserExistAndPasswordCorrect: isUserExistAndPasswordCorrect, 
