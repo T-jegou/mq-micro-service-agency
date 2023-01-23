e035f52d235945b084ee6c3a20a615d52227b976
T-jegou <thomas.jegou1@gmail.com>
2023-01-23 20:21:25 +0100

    Data model user for orm

---
+++ Agent-service/src/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  surname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  zipCode: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  reservationsID: {
+    type: Array
+  },
+});
+
+module.exports = {
+    userSchema: userSchema
+} 
