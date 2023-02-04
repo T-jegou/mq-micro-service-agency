a85e1b038e4c40dd16537636196c691f8f81193b
T-jegou <thomas.jegou1@gmail.com>
2023-02-04 18:14:01 +0100

    Agent can list cars

--- Agent-service/src/routes/carSystemRouter.js
+++ Agent-service/src/routes/carSystemRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {AddCarToCalatog, CreateReservationFromAgency, isThisCarAvaible, listReservation} = require ('../controllers/carController');
+const {AddCarToCalatog, CreateReservationFromAgency, isThisCarAvaible, listReservation, listCars} = require ('../controllers/carController');
 const { validAddCarToCatalog, validCreateReservation, validIsAvailable, validIsAgent } = require('../lib/validator');
 
 const router = express.Router();
 
 router.post('/catalog' , validAddCarToCatalog, AddCarToCalatog);
+router.get('/catalog', validIsAgent, listCars)
 router.post('/reservation', validCreateReservation, CreateReservationFromAgency )
 router.get('/availability', validIsAvailable, isThisCarAvaible)
 router.get('/listReservation', validIsAgent, listReservation)
