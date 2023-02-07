0d12602b18a90ecc48d2eec2756c43eaa0c4a653
T-jegou <thomas.jegou1@gmail.com>
2023-02-07 13:07:11 +0100

    Add more sleep time for service wait before Mongo and MQ up

--- Agent-service/index.js
+++ Agent-service/index.js
@@ -1,6 +1,6 @@
 const { startServer } = require('./src/app');
 const { logger } = require('./src/services/loggerService')
-const SLEEP_TIME = process.env.SLEEP_TIME || 1000;
+const SLEEP_TIME = process.env.SLEEP_TIME || 10000;
 
 // sleep till MongoDB and RabbitMQ services start
 logger.info(`Sleeping for ${SLEEP_TIME}ms before connecting to MongoDB and RabbitMQ.`)
